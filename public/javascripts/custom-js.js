535ce3e5030e75ad012469e267a29df5ac003f8a
dashcraft <ashcraftbookkeeping@gmail.com>
2016-06-26 10:16:06 -0500

    Added images and slick.js slider

--- public/javascripts/custom-js.js
+++ public/javascripts/custom-js.js
@@ -2,9 +2,27 @@ $(document).ready(function(){
 
 $('#new-dropdown').hide();
 
+
+
 $('#dropdown').on('click',function(){
 	$('#new-dropdown').toggle(700);
 
 })
+
+$('.main-background-slider').slick({ 
+	slidesToShow: 1,
+  	slidesToScroll: 1,
+  	autoplay: true,
+  	autoplaySpeed: 2000,
+	infinite: true,
+	speed: 300,
+	centerMode: true,
+	variableWidth: true,
+	arrows:false,
+	
+})
+
+
+
 
 })
